50e177657b089eb0e0faf07679f2bf1711be8edb
Isaac Pinheiro <isaacpnhr@gmail.com>
2022-06-13 19:56:06 -0300

    Mudando a estrutura do código.

--- app.py
+++ app.py
@@ -3,7 +3,43 @@
 
 import netCDF4 as nc
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 import os
+
+class Challenge():
+
+    def __init__(self, pred_ds, obs_ds):
+
+        self.pred_ds = pred_ds
+        self.obs_ds = obs_ds
+
+        self.ntime = len(pred_ds['time'][:])
+        self.nlat = len(pred_ds['lat'][:])
+        self.nlon = len(pred_ds['lon'][:])
+        self.ninterval = int(self.ntime / 6)
+
+        self.t2m = np.array(pred_ds['t2m'][:])
+        self.temp = np.array(obs_ds['temperatura'][:]) + 273.15
+
+        self.rmse = np.empty((self.ninterval, self.nlat, self.nlon))
+
+    def calculate_rmse(self):
+
+        for i in range(0, self.nlat):
+
+            for j in range(0, self.nlon):
+
+                for k in range(0, self.ninterval):
+
+                    self.rmse[k][i][j] = (((self.t2m[(k*6):((k+1)*6), i, j] - self.temp[(k*6):((k+1)*6), i, j]) ** 2).sum() / 6) ** 0.5
+
+    def plot(self):
+
+        sao_paulo = self.rmse[:, 8, 26]
+        print(sao_paulo)
+
+        # TODO
 
 pred_file = os.path.abspath('.') + '/forecast.nc'
 obs_file = os.path.abspath('.') + '/observation.nc'
@@ -11,25 +47,7 @@ obs_file = os.path.abspath('.') + '/observation.nc'
 pred_ds = nc.Dataset(pred_file)
 obs_ds = nc.Dataset(obs_file)
 
-time = len(pred_ds['time'][:])
-lat = len(pred_ds['lat'][:])
-lon = len(pred_ds['lon'][:])
-interval = int(time / 6)
-
-t2m = np.array(pred_ds['t2m'][:])
-temp = np.array(obs_ds['temperatura'][:]) + 273.15
-
-rmse = np.empty((interval, lat, lon))
-
-for i in range(0, lat):
-
-    for j in range(0, lon):
-
-        for k in range(0, interval):
-
-            rmse[k][i][j] = (((t2m[(k*6):((k+1)*6), i, j] - temp[(k*6):((k+1)*6), i, j]) ** 2).sum() / 6) ** 0.5
-
-sao_paulo = rmse[:, 8, 26]
-print(sao_paulo)
-
+ch = Challenge(pred_ds, obs_ds)
+ch.calculate_rmse()
+ch.plot()
 
