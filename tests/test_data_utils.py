f968018e053375306e51c218b2492dd54678b26f
luizamfsantos <luiza.marques_@hotmail.com>
2023-10-26 14:46:53 -0300

    Add unit tests for time_to_datetime function

--- tests/test_data_utils.py
+++ tests/test_data_utils.py
@@ -3,3 +3,32 @@ import os
 
 # Add the path to your project directory
 sys.path.append(os.path.abspath('.'))
+
+import unittest
+from src.data_utils import time_to_datetime, celsius_to_kelvin, rse, rmse_6hrs
+
+import unittest
+import pandas as pd
+from datetime import datetime
+
+class TestTimeToDatetime(unittest.TestCase):
+
+    def test_output_type(self):
+        df = pd.DataFrame({'time': [0, 1, 2]})
+        result = time_to_datetime(df['time'])
+        self.assertIsInstance(result, pd.Series)
+
+    def test_output_values(self):
+        df = pd.DataFrame({'time': [0, 1, 2]})
+        result = time_to_datetime(df['time'])
+        expected = pd.Series(pd.to_datetime(['2018-04-14 00:00:00', '2018-04-14 01:00:00', '2018-04-14 02:00:00']),name="time")
+        pd.testing.assert_series_equal(result, expected)
+
+    def test_custom_reference_date(self):
+        df = pd.DataFrame({'time': [0, 1, 2]})
+        result = time_to_datetime(df['time'], reference_date='2020-01-01')
+        expected = pd.Series(pd.to_datetime(['2020-01-01 00:00:00', '2020-01-01 01:00:00', '2020-01-01 02:00:00']),name="time")
+        pd.testing.assert_series_equal(result, expected)
+
+if __name__ == '__main__':
+    unittest.main()
